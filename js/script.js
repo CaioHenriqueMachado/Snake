77fe54a714b19c9a8581c9bbacddd85b6c760683
Caio <caio.student@outlook.com>
2020-08-09 21:43:41 -0300

    reajuste

--- js/script.js
+++ js/script.js
@@ -207,10 +207,6 @@ function createButton(app){
   div.setAttribute('class', 'buttons');
   app.appendChild(div);
 
-  const divrest = document.createElement('div');
-  div.setAttribute('class', 'buttonRestart');
-  app.appendChild(div);
-
   const up = document.createElement('button');
   up.setAttribute('type', 'button');
   up.setAttribute('id', 'up');
@@ -243,7 +239,7 @@ function createButton(app){
   restart.setAttribute('type', 'button');
   restart.setAttribute('id', 'restart');
   restart.textContent = 'RESTART';
-  divrest.appendChild(restart);
+  div.appendChild(restart);
 
 
 
